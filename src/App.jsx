dd98a06499a36853ad6aafeca2f7abff99b2f912
Muhammad Dzulfiqar Rafli Anwar <rafli.anwar2001@gmail.com>
2024-08-28 17:03:11 +0700

    integrate news api

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
+import { useEffect, useState } from "react";
 import Menubar from "./components/Menubar";
 import Navbar from "./components/Navbar";
+import axios from "axios";
 
 
 function App() {
+  const [news, setNews] = useState([]);
+  console.log('news: ', news);
+
+  useEffect(() => {
+    getNews();
+  }, []);
+
+  const getNews = async () => {
+    try{
+      const response = await axios.get('https://newsapi.org/v2/everything?q=tesla&from=2024-07-28&sortBy=publishedAt&apiKey=d4f66f2a1aa640a38595c8af2b92d6e2');
+      setNews(response.data.articles)
+    } catch(error){
+      console.log(error)  
+    }
+  }
+
   return (
     <>
-      <div className="bg-gray-100 min-h-screen">
-      <Navbar />
-      <Menubar/>
+      <div className='bg-gray-100 min-h-screen'>
+        <Navbar />
+        <Menubar/>
+          <div className='pt-5 pb-28'>
+              <div className='bg-white p-4 rounded-lg mx-28'>
+                {news.map((item, index) => (
+                  <a key={index} href={item?.url} target='_blank'>
+                  <div key={index} className="flex space-x-4 p-4 border-b border-gray-300">
+                    <img src={item?.urlToImage} alt={item?.title} className="w-24 h-24 object-center rounder" />
+                    <div>
+                      <h2 className='font-semibold text-lg'>{item?.title}</h2>
+                      <p className='text-md text-gray-600'>{item?.description}</p>
+                    </div>
+                  </div>
+                  </a>
+                ))}
+              </div>
+          </div>
       </div>
     </>
-  );
+  )
 }
 
 export default App;
