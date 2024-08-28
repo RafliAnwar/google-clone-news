e67d7ee05e47dcc8a41eb6169887ef9bf73c1f76
Muhammad Dzulfiqar Rafli Anwar <rafli.anwar2001@gmail.com>
2024-08-28 14:05:27 +0700

    add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <div className="bg-gray-100 min-h-screen">
+      <Navbar />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
