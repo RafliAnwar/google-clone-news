c8f844138271cf33495a7a9688fbae91ab8bdd59
Muhammad Dzulfiqar Rafli Anwar <rafli.anwar2001@gmail.com>
2024-08-28 14:18:36 +0700

    add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Menubar from "./components/Menubar";
 import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <>
       <div className="bg-gray-100 min-h-screen">
       <Navbar />
+      <Menubar/>
       </div>
     </>
   );
